4aea12b4db920ae9704ac6631b18e54d1120620f
Konstantin Müller <konstantin@mnemonic.no>
2016-12-12 09:45:45 +0100

    Merge pull request #10 in JOSS/commons from ~JOAKIM/commons:feature/ARGUS-6927-mockitotools to master
    Squashed commit of the following:
    
    commit 2dea4cf7eaac079c3ddd581aba21be84d580e730
    Author: Joakim von Brandis <joakim@mnemonic.no>
    Date:   Mon Dec 12 08:23:08 2016 +0100
    
        Updated javadoc
    
    commit 4d36f1da2accbba12bf67d22a96e8fc8a3cf00dd
    Author: Joakim von Brandis <joakim@mnemonic.no>
    Date:   Sat Dec 10 21:53:15 2016 +0100
    
        PR update
    
    commit 49680ae1934f606d24ec6948b607a44b3f41077d
    Author: Joakim von Brandis <joakim@mnemonic.no>
    Date:   Thu Dec 8 11:17:07 2016 +0100
    
        Added mockitotools

---
+++ testtools/src/no/mnemonic/commons/testtools/MockitoTools.java
@@ -0,0 +1,58 @@
+package no.mnemonic.commons.testtools;
+
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
+
+import java.util.function.Predicate;
+
+public class MockitoTools {
+
+  /**
+   * Simplify testing with mockito and argThat by doing
+   * <p>
+   * verify(mock).method(match(arg->arg.isSomething()));
+   *
+   * @param predicate predicate to match the expected argument
+   * @param <T>       the argument type
+   * @return a mock reporter which matches the argument using this predicate
+   */
+  public static <T> T match(Predicate<T> predicate) {
+    return Mockito.argThat(new ArgMatcher<>(predicate));
+  }
+
+  /**
+   * Same as {@link #match(Predicate)}, but with a specific expected argument class, to use if the verified
+   * method takes a superclass of expected argument
+   *
+   * verify(mock).method(match(c->c.getValue()==expectedValue, ExpectedClass.class))
+   * @param expectedClass Expected argument class
+   * @param predicate predicate to test on the argument
+   * @param <T> mocked method argument type
+   * @param <U> expected argument type, subclass of T
+   * @return true if argument is instanceof U and passes the predicate test
+   */
+  public static <T, U extends T> T match(Predicate<T> predicate, Class<U> expectedClass) {
+    return Mockito.argThat(new ArgMatcher<>(o -> {
+      if (!expectedClass.isInstance(o)) return false;
+      //noinspection unchecked
+      U target = (U) o;
+      return predicate.test(target);
+    }));
+  }
+
+  //helpers
+
+  private static class ArgMatcher<T> implements ArgumentMatcher<T> {
+
+    final Predicate<T> predicate;
+
+    ArgMatcher(Predicate<T> predicate) {
+      this.predicate = predicate;
+    }
+
+    @Override
+    public boolean matches(T t) {
+      return predicate.test(t);
+    }
+  }
+}
